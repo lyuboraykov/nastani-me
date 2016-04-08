66a3d1beeba16218ba25c19367821563bae196de
Martin R. Hristov <martin.r.hristov@sap.com>
2016-04-09 02:48:44 +0300

    Home page enhanced + asside has interaction. TODO: fix performance of animation with css transforms

--- platforms/browser/www/js/home.js
+++ platforms/browser/www/js/home.js
@@ -1,3 +1,38 @@
 (function() {
-	
+    var navIcons = document.getElementsByClassName("menu-icon");
+    var profile = document.getElementById("user-icon");
+    [].forEach.call(navIcons, function(el) {
+        el.addEventListener("click", function(event) {
+            if (event.target.id === "user-icon") {
+                return;
+            }
+            document.getElementsByClassName("reds-clicked")[0].classList.remove("reds-clicked");
+            this.classList.add("reds-clicked");
+
+            if (!(this.id === "user-menu")) {
+                document.getElementById("user-menu").classList.remove("animate-user");
+            }
+        });
+    });
+
+    profile.addEventListener("click", function() {
+        document.getElementById("user-menu").classList.add("animate-user");
+        document.getElementById("home-main").classList.add("blur-page");
+        setTimeout(function() {
+            document.getElementById("home-main").addEventListener("click", blurListener);
+        }, 0);
+    });
+
+
+    function blurListener(event) {
+        document.getElementById("home-main").removeEventListener("click", blurListener);
+
+        if (event.target.id !== "user-menu") {
+            document.getElementById("user-menu").classList.remove("animate-user");
+            document.getElementById("home-main").classList.remove("blur-page");
+        } else {
+            return;
+        }
+    }
+
 })();
